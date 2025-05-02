84f4e6c9033ba30a36e1fce0865a8b408f86a7e3
rami3l <rami3l@outlook.com>
2025-05-02 11:15:36 +0800

    Introduce snapshot tests

--- tests/spiro_to_beziers.rs
+++ tests/spiro_to_beziers.rs
@@ -1,5 +1,9 @@
 use spiro_sys::{self, bezctx};
-use std::os::raw::{c_char, c_int};
+
+use std::{
+    fmt::Write as _,
+    os::raw::{c_char, c_int},
+};
 
 macro_rules! spiro_cp {
     ({$x:literal, $y:literal, $ty:literal}) => {
@@ -11,25 +15,114 @@ macro_rules! spiro_cp {
     };
 }
 
-unsafe extern "C" fn println_moveto(_bc: *mut bezctx, x: f64, y: f64, _is_open: c_int) {
-    println!("M {}, {} ", x, y);
+trait BezCtx {
+    fn moveto(&mut self, x: f64, y: f64, is_open: c_int);
+    fn lineto(&mut self, x: f64, y: f64);
+    fn quadto(&mut self, x1: f64, y1: f64, x2: f64, y2: f64);
+    fn curveto(&mut self, x1: f64, y1: f64, x2: f64, y2: f64, x3: f64, y3: f64);
+    fn mark_knot(&mut self, knot_idx: c_int);
 }
-unsafe extern "C" fn println_lineto(_bc: *mut bezctx, x: f64, y: f64) {
-    println!("L {}, {} ", x, y);
+
+#[repr(C)]
+struct BezCtxAdapter<T> {
+    pub raw: bezctx,
+    pub data: T,
 }
-unsafe extern "C" fn println_quadto(_bc: *mut bezctx, x1: f64, y1: f64, x2: f64, y2: f64) {
-    println!("Q {}, {}, {}, {} ", x1, y1, x2, y2);
+
+impl<T: BezCtx> BezCtxAdapter<T> {
+    fn new(data: T) -> Self {
+        BezCtxAdapter {
+            raw: bezctx {
+                moveto: Some(Self::_moveto),
+                lineto: Some(Self::_lineto),
+                quadto: Some(Self::_quadto),
+                curveto: Some(Self::_curveto),
+                mark_knot: Some(Self::_mark_knot),
+            },
+            data,
+        }
+    }
+
+    unsafe extern "C" fn _moveto(bc: *mut bezctx, x: f64, y: f64, is_open: c_int) {
+        unsafe {
+            let this = &mut *(bc as *mut Self);
+            this.data.moveto(x, y, is_open);
+        }
+    }
+
+    unsafe extern "C" fn _lineto(bc: *mut bezctx, x: f64, y: f64) {
+        unsafe {
+            let this = &mut *(bc as *mut Self);
+            this.data.lineto(x, y);
+        }
+    }
+
+    unsafe extern "C" fn _quadto(bc: *mut bezctx, x1: f64, y1: f64, x2: f64, y2: f64) {
+        unsafe {
+            let this = &mut *(bc as *mut Self);
+            this.data.quadto(x1, y1, x2, y2);
+        }
+    }
+
+    unsafe extern "C" fn _curveto(
+        bc: *mut bezctx,
+        x1: f64,
+        y1: f64,
+        x2: f64,
+        y2: f64,
+        x3: f64,
+        y3: f64,
+    ) {
+        unsafe {
+            let this = &mut *(bc as *mut Self);
+            this.data.curveto(x1, y1, x2, y2, x3, y3);
+        }
+    }
+
+    unsafe extern "C" fn _mark_knot(bc: *mut bezctx, knot_idx: c_int) {
+        unsafe {
+            let this = &mut *(bc as *mut Self);
+            this.data.mark_knot(knot_idx);
+        }
+    }
 }
-unsafe extern "C" fn println_curveto(
-    _bc: *mut bezctx,
-    x1: f64,
-    y1: f64,
-    x2: f64,
-    y2: f64,
-    x3: f64,
-    y3: f64,
-) {
-    println!("C {}, {}, {}, {}, {}, {} ", x1, y1, x2, y2, x3, y3);
+
+#[derive(Debug, Default, Clone)]
+struct TestBezCtx {
+    buf: String,
+}
+
+impl TestBezCtx {
+    const PRECISION: usize = 12;
+}
+
+impl BezCtx for TestBezCtx {
+    fn moveto(&mut self, x: f64, y: f64, _is_open: c_int) {
+        let p = Self::PRECISION;
+        _ = writeln!(self.buf, "M {x:.p$} {y:.p$}");
+    }
+
+    fn lineto(&mut self, x: f64, y: f64) {
+        let p = Self::PRECISION;
+        _ = writeln!(self.buf, "L {x:.p$} {y:.p$}");
+    }
+
+    fn quadto(&mut self, x1: f64, y1: f64, x2: f64, y2: f64) {
+        let p = Self::PRECISION;
+        _ = writeln!(self.buf, "Q {x1:.p$} {y1:.p$}, {x2:.p$} {y2:.p$}");
+    }
+
+    fn curveto(&mut self, x1: f64, y1: f64, x2: f64, y2: f64, x3: f64, y3: f64) {
+        let p = Self::PRECISION;
+        _ = writeln!(
+            self.buf,
+            "C {x1:.p$} {y1:.p$}, {x2:.p$} {y2:.p$}, {x3:.p$} {y3:.p$}"
+        );
+    }
+
+    fn mark_knot(&mut self, _knot_idx: c_int) {
+        _ = writeln!(self.buf);
+    }
 }
 
 #[test]
@@ -51,15 +144,12 @@ fn spiro_to_beziers() {
         spiro_cp!({-80,-250, '}'}),
     ];
 
-    let mut ctx = bezctx {
-        moveto: Some(println_moveto),
-        lineto: Some(println_lineto),
-        quadto: Some(println_quadto),
-        curveto: Some(println_curveto),
-        mark_knot: None,
-    };
+    let mut ctx = BezCtxAdapter::new(TestBezCtx::default());
 
     unsafe {
-        spiro_sys::TaggedSpiroCPsToBezier(path5.as_mut_ptr(), &mut ctx);
+        spiro_sys::TaggedSpiroCPsToBezier(path5.as_mut_ptr(), (&raw mut ctx).cast());
     }
+
+    // You may verify the output at <https://svg-path-visualizer.netlify.app>.
+    insta::assert_snapshot!(&ctx.data.buf);
 }
